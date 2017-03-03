9dadb91e8c007cdcf80c1baa52b67f201640e33f
Jaydon Carling <jaydon134@me.com>
2017-03-03 12:19:53 -0700

    Added style.css file and added function calls for numerical sequences.

--- scratchpad.js
+++ scratchpad.js
@@ -6,8 +6,95 @@
  * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
  * 3. Display to insert the result in a comment after the selection. (Cmd-L)
  */
+//set title
+document.title = 'Jaydon\'s Big Mathinessness';
 
-document.title = "Jaydon's Big Mathinessness";
-
-var primFibDiv = document.createElement("div");
+//create a primary div to hold all the Fib stuff
+var primFibDiv = document.createElement('div');
+primFibDiv.className = 'parentDiv';
 document.body.appendChild(primFibDiv);
+//create a primary div to hold all the Pell stuff
+var primPellDiv = document.createElement('div');
+primPellDiv.className = 'parentDiv';
+document.body.appendChild(primPellDiv);
+//create a primary div to hold all the Tri stuff
+var primTriDiv = document.createElement('div');
+primTriDiv.className = 'parentDiv';
+document.body.appendChild(primTriDiv);
+
+
+//run all three sequences
+// fib(11, primFibDiv);
+// pell(11, primPellDiv);
+// fib(11, primTriDiv);
+
+var fib = function(n, node) {
+  var tree = recursiveFib(n)
+  node.appendChild(tree.html);
+  //node.style = "display: inline-block;";
+  node.setAttribute('id', 'fib');
+}
+
+var recursiveFib = function(n) {
+  var value;
+  var div = document.createElement('div');
+  div.setAttribute('class', 'fib');
+  // leaf nodes aka. base case
+  if (n < 2) {
+    if (n === 0) {
+      value = 0;
+    } 
+    else if (n === 1) {
+      value = 1;
+    }
+    var p = document.createElement('p');
+    p.textContent = 'Fib(' + n + ') = ' + value;
+    div.appendChild(p)
+  } 
+  else{
+    var left = fibHelper(n - 1);
+    var clas = left.html.getAttribute('class');
+    left.html.setAttribute('class', clas + ' fib-left');
+    var right = fibHelper(n - 2);
+    clas = right.html.getAttribute('class');
+    right.html.setAttribute('class', clas + ' fib-right');
+    value = left.value + right.value;
+    var p = document.createElement('p');
+    p.textContent = 'Fib(' + n + ') = ' + value;
+    div.appendChild(p);
+    div.appendChild(left.html);
+    div.appendChild(right.html);
+  }
+  return {
+    'value': value,
+    'html': div
+  };
+}
+
+function pell(n, node){
+  var tree = recursivePell(n);
+  node.appendChild(tree.html);
+  node.setAttribute('id', 'pell');
+}
+
+function recursivePell(n){
+  var value;
+  var pellDiv = document.createElement('div');
+  pellDiv.setAttribute('class', 'pell');
+  if(n < 2){
+    if(n === 0){
+      value = 0;
+    }
+    if(n=== 1){
+      value = 1;
+    }
+    var p = document.createElement('p');
+    p.textContent = 'Pell(' + n + ') = ' + value;
+    pellDiv.appendChild(p);
+  }
+  else{
+    var left = recursivePell(n - 1);
+    var right = recursivePell(n - 2);
+    
+  }
+}
